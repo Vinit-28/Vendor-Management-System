bb426de332be2e96a84726eab165386d5afc2497
Vinit-28 <codervinit@gmail.com>
2023-12-09 00:59:25 +0530

    Updated the Serializers to avoid updating the primary key

--- Vendor_Manager/serializers.py
+++ Vendor_Manager/serializers.py
@@ -9,6 +9,11 @@ class VendorProfileSerializer(ModelSerializer):
     class Meta:
         model = VendorProfileModel
         fields = '__all__'
+    
+    # Overriding the update function to avoid updating the primary key # 
+    def update(self, instance, validated_data):
+        validated_data.pop('vendor_code', None)
+        return super().update(instance, validated_data)
 
 
 
@@ -17,3 +22,8 @@ class PurchaseOrdersSerializer(ModelSerializer):
     class Meta:
         model = PurchaseOrdersModel
         fields = '__all__'
+    
+    # Overriding the update function to avoid updating the primary key # 
+    def update(self, instance, validated_data):
+        validated_data.pop('po_number', None)
+        return super().update(instance, validated_data)
--- Vendor_Manager/views.py
+++ Vendor_Manager/views.py
@@ -57,72 +57,58 @@ class VendorProfileView(APIView):
 
 
     # Put to update the Vendor Profile #
-    def put(self, request, vendor_id=None):
+    def put(self, request, vendor_id):
         response = dict()
         response_status = status.HTTP_200_OK
-        if vendor_id is not None:
-            try:
-                vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
-                serializer = VendorProfileSerializer(vendor_profile, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    response['status'] = 'Success'
-                    response['message'] = 'Vendor Profile Updated Successfully.'
-                    response['Vendor Profile'] = serializer.data
-                else:
-                    response['status'] = 'Failed'
-                    response['Errors'] = serializer.errors
-            except ObjectDoesNotExist as err:
-                response_status = status.HTTP_400_BAD_REQUEST
+        try:
+            vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
+            serializer = VendorProfileSerializer(vendor_profile, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                response['status'] = 'Success'
+                response['message'] = 'Vendor Profile Updated Successfully.'
+                response['Vendor Profile'] = serializer.data
+            else:
                 response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f'No Vendor with Vendor Id "{vendor_id}".']
-                }
-            except Exception as err:
-                response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f"{err}."]
-                }
-        else:
+                response['Errors'] = serializer.errors
+        except ObjectDoesNotExist as err:
             response_status = status.HTTP_400_BAD_REQUEST
             response['status'] = 'Failed'
             response['Errors'] = {
-                'message': [f'Please provide a valid Vendor Id.']
+                'message': [f'No Vendor with Vendor Id "{vendor_id}".']
+            }
+        except Exception as err:
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f"{err}."]
             }
         return Response(response, status=response_status)
 
 
     # Delete to delete the Vendor Profile #
-    def delete(self, request, vendor_id=None):
+    def delete(self, request, vendor_id):
         response = dict()
         response_status = status.HTTP_200_OK
-        if vendor_id is not None:
-            try:
-                vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
-                serializer = VendorProfileSerializer(vendor_profile)
-                vendor_profile_json = serializer.data
-                vendor_profile.delete()
-                response['status'] = 'Success'
-                response['message'] = 'Vendor Profile Deleted Successfully.'
-                response['Vendor Profile'] = vendor_profile_json
-            except ObjectDoesNotExist as err:
-                response_status = status.HTTP_400_BAD_REQUEST
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f'No Vendor with Vendor Id "{vendor_id}".']
-                }
-            except Exception as err:
-                response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f"{err}."]
-                }
-        else:
+        try:
+            vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
+            serializer = VendorProfileSerializer(vendor_profile)
+            vendor_profile_json = serializer.data
+            vendor_profile.delete()
+            response['status'] = 'Success'
+            response['message'] = 'Vendor Profile Deleted Successfully.'
+            response['Vendor Profile'] = vendor_profile_json
+        except ObjectDoesNotExist as err:
             response_status = status.HTTP_400_BAD_REQUEST
             response['status'] = 'Failed'
             response['Errors'] = {
-                'message': [f'Please provide a valid Vendor Id.']
+                'message': [f'No Vendor with Vendor Id "{vendor_id}".']
+            }
+        except Exception as err:
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f"{err}."]
             }
         return Response(response, status=response_status)
 
@@ -156,11 +142,13 @@ class PurchaseOrdersView(APIView):
         return Response(response, status=response_status)
     
 
-    # Post to create the Vendor Profile #
+    # Post to create the Purchase Order #
     def post(self, request):
         response = dict()
         response_status = status.HTTP_200_OK
         try:
+            vendor_id = request.data.get('vendor')
+            vendor = VendorProfileModel.objects.get(pk=vendor_id) # Will raise the exception if vendor doesn't exist #
             serializer = PurchaseOrdersSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
@@ -169,6 +157,12 @@ class PurchaseOrdersView(APIView):
             else:
                 response['status'] = 'Failed'
                 response['Errors'] = serializer.errors
+        except ObjectDoesNotExist as err:
+            response_status = status.HTTP_400_BAD_REQUEST
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f"Please provide a valid vendor id."]
+            }
         except Exception as err:
             response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
             response['status'] = 'Failed'
@@ -178,74 +172,60 @@ class PurchaseOrdersView(APIView):
         return Response(response, status=response_status)
 
 
-    # Put to update the Vendor Profile #
-    def put(self, request, vendor_id=None):
+    # Put to update the Purchase Order #
+    def put(self, request, po_id):
         response = dict()
         response_status = status.HTTP_200_OK
-        if vendor_id is not None:
-            try:
-                vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
-                serializer = VendorProfileSerializer(vendor_profile, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    response['status'] = 'Success'
-                    response['message'] = 'Vendor Profile Updated Successfully.'
-                    response['Vendor Profile'] = serializer.data
-                else:
-                    response['status'] = 'Failed'
-                    response['Errors'] = serializer.errors
-            except ObjectDoesNotExist as err:
-                response_status = status.HTTP_400_BAD_REQUEST
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f'No Vendor with Vendor Id "{vendor_id}".']
-                }
-            except Exception as err:
-                response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f"{err}."]
-                }
-        else:
-            response_status = status.HTTP_400_BAD_REQUEST
-            response['status'] = 'Failed'
-            response['Errors'] = {
-                'message': [f'Please provide a valid Vendor Id.']
-            }
-        return Response(response, status=response_status)
-
-
-    # Delete to delete the Vendor Profile #
-    def delete(self, request, vendor_id=None):
-        response = dict()
-        response_status = status.HTTP_200_OK
-        if vendor_id is not None:
-            try:
-                vendor_profile = VendorProfileModel.objects.get(vendor_code=vendor_id)
-                serializer = VendorProfileSerializer(vendor_profile)
-                vendor_profile_json = serializer.data
-                vendor_profile.delete()
+        try:
+            purchase_order = PurchaseOrdersModel.objects.get(pk=po_id)
+            vendor_id = request.data.get('vendor')
+            vendor = VendorProfileModel.objects.get(pk=vendor_id) if vendor_id else purchase_order.vendor # VAlidating the whether the new/updated vendor exists or not #
+            serializer = PurchaseOrdersSerializer(purchase_order, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
                 response['status'] = 'Success'
-                response['message'] = 'Vendor Profile Deleted Successfully.'
-                response['Vendor Profile'] = vendor_profile_json
-            except ObjectDoesNotExist as err:
-                response_status = status.HTTP_400_BAD_REQUEST
+                response['message'] = 'Purchase Order Updated Successfully.'
+                response['Purchase Order'] = serializer.data
+            else:
                 response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f'No Vendor with Vendor Id "{vendor_id}".']
-                }
-            except Exception as err:
-                response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-                response['status'] = 'Failed'
-                response['Errors'] = {
-                    'message': [f"{err}."]
-                }
-        else:
+                response['Errors'] = serializer.errors
+        except ObjectDoesNotExist as err:
             response_status = status.HTTP_400_BAD_REQUEST
             response['status'] = 'Failed'
             response['Errors'] = {
-                'message': [f'Please provide a valid Vendor Id.']
+                'message': [f'Invalid purchase order id or vendor id.']
+            }
+        except Exception as err:
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f"{err}."]
             }
         return Response(response, status=response_status)
 
 
+    # Delete to delete the Purchase Order #
+    def delete(self, request, po_id):
+        response = dict()
+        response_status = status.HTTP_200_OK
+        try:
+            purchase_order = PurchaseOrdersModel.objects.get(pk=po_id)
+            serializer = PurchaseOrdersSerializer(purchase_order)
+            purchase_order_json = serializer.data
+            purchase_order.delete()
+            response['status'] = 'Success'
+            response['message'] = 'Purchase Order Deleted Successfully.'
+            response['Purchase Order'] = purchase_order_json
+        except ObjectDoesNotExist as err:
+            response_status = status.HTTP_400_BAD_REQUEST
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f'No purchase order with id {po_id}.']
+            }
+        except Exception as err:
+            response_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response['status'] = 'Failed'
+            response['Errors'] = {
+                'message': [f"{err}."]
+            }
+        return Response(response, status=response_status)
